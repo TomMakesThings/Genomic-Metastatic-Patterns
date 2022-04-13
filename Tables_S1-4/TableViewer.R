800b97e57d382ebaa777fd2400dcf7ba424e0da5
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2022-04-13 16:06:22 +0100

    Update TableViewer.R

--- Tables_S1-4/TableViewer.R
+++ Tables_S1-4/TableViewer.R
@@ -3,6 +3,6 @@ library("readxl")
 setwd("C:/Users/redds/Documents/GitHub/Genomics-II-Group/")
 
 # Open table
-table_s2 <- read_excel("Tables_S1-4/Table_S2.xlsx", skip = 2)
+table_s2a <- read_excel("Tables_S1-4/Table_S2.xlsx", sheet = 1, skip = 2)
 
 View(table_s2)
