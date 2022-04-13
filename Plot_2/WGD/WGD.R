ea334a48bb6e1ba6b76a6bcd9e72385687a4e169
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2022-04-13 13:06:30 +0100

    Add text file

--- Plot_2/WGD/WGD.R
+++ Plot_2/WGD/WGD.R
@@ -3,7 +3,7 @@ library(facetsSuite)
 
 setwd("C:/Users/redds/Documents/GitHub/Genomics-II-Group/")
 
-# View table
+# View S2A table
 table_s2a <- read_excel("Tables_S1-4/Table_S2.xlsx", sheet = 1, skip = 2)
 
 msk_met <- read.table(file = "Plot_2/WGD/msk_met_2021_clinical_data.tsv",
@@ -17,13 +17,35 @@ message(paste("Number of metastatic samples: ", nrow(metastatic_samples)))
 
 # Find samples with FACETS 
 facets_fga_df <- table_s2a[which(table_s2a$alteration == "fga_facets"), ]
-sum(facets_fga_df$primary_n) + sum(facets_fga_df$metastasis_n)
+message(paste("Found", sum(facets_fga_df$primary_n) + sum(facets_fga_df$metastasis_n),
+              "samples with FACETS data"))
+
+facets_fga_df$tumor_type
+
+table_s2a
+
+
+
+# Remove NA columns from dataframe
+removeEmptyCols <- function(df) {
+  return(df[ ,colSums(is.na(df)) < nrow(df)])
+}
+
+
 
 # Open mutation data
-mutations <- read.table(file = "Plot_2/WGD/data_mutations.txt", 
+mutations <- removeEmptyCols(read.table(file = "Data/data_mutations.txt", 
+                                        header = TRUE, fill = TRUE))
+
+cna <- read.table(file = "Data/data_cna.txt", 
                         header = TRUE, fill = TRUE)
-# Remove empty columns
-mutations <- mutations[ ,colSums(is.na(mutations)) < nrow(mutations)]
+
+cna_hg19 <- read.table(file = "Data/data_cna_hg19.seg", 
+                       header = TRUE, fill = TRUE)
+
+sv <- read.table(file = "Data/data_sv.txt", sep = '\t', quote = "",
+                       header = TRUE, fill = TRUE)
+
 
 library(pctGCdata)
 test_read_counts
---
+++ Text/README.md
@@ -0,0 +1,3 @@
+# Genomics-II-Group
+
+Paper: [Genomic characterization of metastatic patterns from prospective clinical sequencing of 25,000 patients](https://www.researchgate.net/publication/352897071_Genomic_characterization_of_metastatic_patterns_from_prospective_clinical_sequencing_of_25000_patients)
