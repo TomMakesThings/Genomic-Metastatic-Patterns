d88482db2e54fb9cdd7156b82d6f24266d0534d6
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2022-04-13 15:45:06 +0100

    Update RunASCETS.R

--- Plot_2/aSCNAs/RunASCETS.R
+++ Plot_2/aSCNAs/RunASCETS.R
@@ -1,3 +1,5 @@
+# Run ASCETS, the arm-level copy number events caller for targeted sequencing data
+
 setwd("C:/Users/redds/Documents/GitHub/Genomics-II-Group/")
 
 # Import ASCETS functions from script
@@ -5,8 +7,9 @@ source("ASCETS/ascets_resources.R")
 
 # Open CNA segmentation file
 cna_hg19 <- read.table(file = "Data/data_cna_hg19.seg", header = TRUE)
+# Open chromosome arm genomic coordinates for reference hg19
+cytoband_hg19 <- read.table(file = "ASCETS/cytoband_coordinates_hg19.txt", header = TRUE)
 
-# 
-ascets_output <- ascets(cna = cna_hg19, cytoband = , 
-                        min_cov = 0.5)
+# Run ASCETS
+ascets_output <- ascets(cna = cna_hg19, cytoband = cytoband_hg19)
 write_outputs_to_file(ascets_output, location = "output_folder/")
