51f9a4cce87ad9de18130721a8d3141f6e999d0d
TomMakesThings <61354833+TomMakesThings@users.noreply.github.com>
2022-04-14 15:54:10 +0100

    Add ASCETS results

--- Plot_2/aSCNAs/RunASCETS.R
+++ Plot_2/aSCNAs/RunASCETS.R
@@ -8,8 +8,8 @@ source("ASCETS/ascets_resources.R")
 # Open CNA segmentation file
 cna_hg19 <- read.table(file = "Data/data_cna_hg19.seg", header = TRUE)
 # Open chromosome arm genomic coordinates for reference hg19
-cytoband_hg19 <- read.table(file = "ASCETS/cytoband_coordinates_hg19.txt", header = TRUE)
+cytoband_hg19 <- read.table(file = "ASCETS/genomic_arm_coordinates_hg19.txt", header = TRUE)
 
 # Run ASCETS
-ascets_output <- ascets(cna = cna_hg19, cytoband = cytoband_hg19)
-write_outputs_to_file(ascets_output, location = "output_folder/")
+ascets_output <- ascets(cna = cna_hg19, cytoband = cytoband_hg19, name = "ascets_results")
+write_outputs_to_file(ascets_output, location = "Plot_2/aSCNAs")
