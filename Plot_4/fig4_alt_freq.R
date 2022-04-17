94f05b255cf1ea3e17535ed624982fe3f3d96baf
Ragnar1999 <825820189@qqccom>
2022-04-17 21:57:11 +0100

    dataframe done for figure 4A, not yet plotted(soon)

---
+++ Plot_4/fig4_alt_freq.R
@@ -0,0 +1,112 @@
+library(readxl)
+library(DescTools)
+library(ggplot2)
+
+
+
+samples_data <- read.csv(file = "Plot_2/FGA/calculated_TMB_and_FGA.csv",
+                         header = TRUE, fill = TRUE)
+table_s1a <- read_excel("Tables_S1-4/Table_S1.xlsx", sheet = 1, skip = 2)
+table_s2a <- read_excel("Tables_S1-4/Table_S2.xlsx", sheet = 1, skip = 2)
+
+#all cancer types
+cancer_subtypes <- unique(table_s4b$tumor_type)
+cancer_subtypes
+#filter by qvalue
+table_s2a_sig_fga<-table_s2a %>% filter(alteration=='fga') %>% filter(qval<0.05)
+table_s2a_sig_fga
+
+
+#tumor_type_sig_fga<-unique(table_s2a_sig_fga$tumor_type)
+tumor_type_sig_fga<-c("lung_adenocarcinoma","colorectal_cancer_mss","prostate_adenocarcinoma",
+                      "ILC_HR+HER2-","pancreatic_adenocarcinoma", "head_and_neck_squamous",
+                      "melanoma_cutaneous","thyroid_papillary","esophageal_adenocarcinoma",
+                      "adenoid_cystic_carcinoma","lung_neuroendocrine" ,"mesothelioma_pleural",
+                      "sarcoma_ups/mxf"
+                      )
+subtype_colour=c()
+for (subtype in tumor_type_sig_fga) {
+    subtype_colour<-c(subtype_colour,table_s1a[which(table_s1a$curated_subtype == subtype), ]$color_subtype)
+}
+# a dataframe of tumor_type(sig) and corresponding color 
+significant_tumor_type=data.frame(tumor_type=tumor_type_sig_fga,colour=subtype_colour)
+significant_tumor_type
+
+
+#input data for figure 4
+table_s4b <- read_excel("Tables_S1-4/Table_S4.xlsx", sheet = 2, skip = 2)
+target_organ<-unique(table_s4b$TO)
+length(target_organ)
+target_organ
+
+#classify alternation
+alternation<-unique(table_s4b$alteration)
+library(stringr)
+alternation_updated=alternation[which(str_detect(alternation,pattern="gain")==FALSE)]
+alternation_updated=alternation_updated[which(str_detect(alternation_updated,pattern="loss")==FALSE)]
+alternation_updated=alternation_updated[which(str_detect(alternation_updated,pattern="fga")==FALSE)]
+alternation_updated=alternation_updated[which(str_detect(alternation_updated,pattern="tmb")==FALSE)]
+mut<-alternation_updated[which(str_detect(alternation_updated,pattern="_mut")==TRUE)]
+Amplification<-alternation_updated[which(str_detect(alternation_updated,pattern="_Amplification")==TRUE)]
+Deletion<-alternation_updated[which(str_detect(alternation_updated,pattern="_Deletion")==TRUE)]
+fusion<-alternation_updated[which(str_detect(alternation_updated,pattern="_fusion")==TRUE)]
+alternation_updated
+
+pathway=alternation_updated[!alternation_updated %in% c(mut,Amplification,Deletion,fusion)]
+non_pathway=c(mut,Amplification,Deletion,fusion)
+
+figure4_plot_info=data.frame()
+
+alt=NA
+TO=NA
+alt_freq_low=NA
+alt_freq_high=NA
+bg_color=NA
+triangle_color=NA
+
+
+#assign color to mut,amp,del,fus,pathway
+#green,red,blue,purple,navy respectively
+
+map_color=function(s){
+  if (s %in% mut){c='green'}
+  if (s %in% Amplification){c='red'}
+  if (s %in% Deletion){c='blue'}
+  if (s %in% fusion){c='purple'}
+  if (s %in% pathway){c='navy'}
+  return(c)
+}
+
+
+for (i in 1:length(significant_tumor_type$tumor_type)){
+  temp=table_s4b[(table_s4b$tumor_type==significant_tumor_type$tumor_type[i]),]
+  temp=temp %>%filter(qval<0.05)
+  #non_pathway come first(italic font in graph)
+  for (p in 1:length(temp$tumor_type)){
+    if (((temp$alteration[p] %in% non_pathway)==TRUE) && ((temp$TO[p] %in% target_organ)==TRUE)){
+        alt=c(alt,temp$alteration[p])
+        TO=c(TO,temp$TO[p])
+        alt_freq_low=c(alt_freq_low,temp$noTO_pc[p])
+        alt_freq_high=c(alt_freq_high,temp$TO_pc[p])
+        bg_color=c(bg_color,significant_tumor_type$colour[i])
+        triangle_color=c(triangle_color,map_color(temp$alteration[p]))
+    }
+    #pathway(regular font, under nonpathway)
+  }
+  for (p in 1:length(temp$tumor_type)){
+    if (((temp$alteration[p] %in% pathway)==TRUE) && ((temp$TO[p] %in% target_organ)==TRUE)){
+      alt=c(alt,temp$alteration[p])
+      TO=c(TO,temp$TO[p])
+      alt_freq_low=c(alt_freq_low,temp$noTO_pc[p])
+      alt_freq_high=c(alt_freq_high,temp$TO_pc[p])
+      bg_color=c(bg_color,significant_tumor_type$colour[i])
+      triangle_color=c(triangle_color,map_color(temp$alteration[p]))
+    }
+  }
+}
+
+
+figure4_plot_info=data.frame(alternation=alt,TO=TO,alternation_freq1=alt_freq_low,alternation_freq2=alt_freq_high,
+                             bg_color=bg_color,triangle_color=triangle_color)
+figure4_plot_info
+write.csv(figure4_plot_info, file = "Plot_4/figure4_plot_info.csv",row.names = FALSE)
